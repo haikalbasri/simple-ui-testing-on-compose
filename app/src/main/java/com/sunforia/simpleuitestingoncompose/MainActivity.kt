90b8b31cb2e14f90967c4b2f19d6392f0a8e73ee
haikalbasri <basribusrut93@gmail.com>
2024-07-24 12:19:28 +0700

    ui simple testing

---
+++ app/src/main/java/com/sunforia/simpleuitestingoncompose/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.sunforia.simpleuitestingoncompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.sunforia.simpleuitestingoncompose.ui.theme.SimpleUITestingOnComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SimpleUITestingOnComposeTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    CalculatorApp()
+                }
+            }
+        }
+    }
+}
